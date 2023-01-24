50615135e836ea8839488e13b85581da1e4016e1
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-01-24 14:42:48 +0530

    Delete matrices/lines/12.11.3.2 directory

--- matrices/lines/12.11.3.2/12.11.3.2/code/unit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Define the vector
-n= np.array([3, 5, -6])
-
-# Find the magnitude of the vector
-c1 = (np.linalg.norm(n))*7
-
-print(c1)
--- matrices/lines/12.11.3.2/code/unit.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Define the vector
-C = np.array([-18, 12, -4])
-
-# Find the magnitude of the vector
-c1 = (1/np.linalg.norm(C))*C
-
-print(c1)
