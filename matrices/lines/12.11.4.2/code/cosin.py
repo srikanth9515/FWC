7571baa2716f3269172a64e9016e055025c5bd44
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-01-27 14:39:15 +0530

    Add files via upload

--- matrices/lines/12.11.4.2/code/cosin.py
+++ matrices/lines/12.11.4.2/code/cosin.py
@@ -7,8 +7,8 @@ n1=sym.Symbol('n1')
 l2=sym.Symbol('l2')
 m2=sym.Symbol('m2')
 n2=sym.Symbol('n2')
-A=np.array([l1,m1,n1])
-B=np.array([l2,m2,n2])
-res=np.cross(A,B)
-
-print("The vector perpendicular to both the vectors is : {}".format(res))
+A=np.array(([l1,m1,n1],[l2,m2,n2],[m1*n2 - m2*n1,-l1*n2 + l2*n1,l1*m2 - l2*m1]))
+B=np.transpose(A)
+C=A@B
+l1**2+m1**2+n1**2==0
+print(C)
