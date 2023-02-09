27e5a53833a2de710d7bc7f1447758113b4ea61d
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-02-09 12:09:53 +0530

    Add files via upload

---
+++ vectors/10/7.2/6/c codes_2/2.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int solve(int a,int b,int c)
+{
+
+	return (c-b)/a;
+}
+
+
+
+
+void pmf(char *str, int *a,int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%d\n",a[i]);
+}
+}
+
+
+
+int main()
+{
+char a[10]="1,2";
+char b[10]="4,y";
+char c[10]="3,5";
+char d[10]="x,6";
+printf("a=%s",a);
+printf("\n");
+printf("b=%s",b);
+printf("\n");
+printf("c=%s",c);
+printf("\n");
+printf("d=%s",d);
+printf("\n");
+int a1[10]={1,2};
+int b1[10]={4,3};
+int c1[10]={3,5};
+int d1[10]={6,6};
+pmf ("a.dat",a1,2);
+pmf ("b.dat",b1,2);
+pmf ("c.dat",c1,2);
+pmf ("d.dat",d1,2);
+
+
+printf("Solve for x :");
+int e,f,g;
+e=1;
+f=-3;
+g=3;
+int x=solve(e,f,g);
+printf("%d",x);
+printf("\n");
+printf("solve for y : ");
+int l=1,m=-2,n=1;
+int y=solve(l,m,n);
+printf("%d",y);
+
+
+return 0;
+}
+
