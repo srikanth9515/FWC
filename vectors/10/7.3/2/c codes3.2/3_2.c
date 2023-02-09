361a360b3c42b8be053290c8bb15e88782932151
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-02-09 12:11:10 +0530

    Add files via upload

---
+++ vectors/10/7.3/2/c codes3.2/3_2.c
@@ -0,0 +1,79 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int solve(int a,int b,int c)
+{
+
+	return (c-b)/a;
+}
+
+
+
+
+void pmf(char *str, int *a,int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%d\n",a[i]);
+}
+}
+
+
+
+int main()
+{
+int a[10]={8,1};
+int b[10]={3,-4};
+char c[10]="k,-4";
+int i;
+for(i=0;i<2;i++)
+{
+	printf("%d\n",a[i]);
+}
+printf("\n");
+for(i=0;i<2;i++)
+{
+	printf("%d\n",b[i]);
+}
+printf("\n");
+printf("c=%s",c);
+printf("\n");
+
+
+int d[10];
+
+for(i=0;i<2;i++)
+{
+	d[i]=b[i]-a[i];
+}
+
+printf("\n");
+
+for(i=0;i<2;i++)
+{
+	printf("%d\n",d[i]);
+}
+printf("Solve for k :");
+int e,f,g;
+e=-1;
+f=8;
+g=6;
+int x=solve(e,f,g);
+printf("%d",x);
+printf("\n");
+
+int c1[10]={2,-5};
+
+pmf("a.dat",a,2);
+pmf("b.dat",b,2);
+pmf("c.dat",c1,2);
+
+
+return 0;
+}
---
+++ vectors/10/7.3/2/c codes_3.1/3.c
@@ -0,0 +1,77 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int solve(int a,int b,int c)
+{
+
+	return (c-b)/a;
+}
+
+
+
+void pmf(char *str, int *a,int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%d\n",a[i]);
+}
+}
+
+
+
+int main()
+{
+int a[10]={7,-2};
+int b[10]={5,1};
+char c[10]="3,k";
+int i;
+for(i=0;i<2;i++)
+{
+	printf("%d\n",a[i]);
+}
+printf("\n");
+for(i=0;i<2;i++)
+{
+	printf("%d\n",b[i]);
+}
+printf("\n");
+printf("c=%s",c);
+printf("\n");
+
+
+int d[10];
+
+for(i=0;i<2;i++)
+{
+	d[i]=b[i]-a[i];
+}
+
+printf("\n");
+
+for(i=0;i<2;i++)
+{
+	printf("%d\n",d[i]);
+}
+printf("Solve for k :");
+int e,f,g;
+e=1;
+f=-1;
+g=3;
+int x=solve(e,f,g);
+printf("%d",x);
+printf("\n");
+int c1[10]={3,4};
+
+pmf("a.dat",a,2);
+pmf("b.dat",b,2);
+pmf("c.dat",c1,2);
+
+
+return 0;
+}
