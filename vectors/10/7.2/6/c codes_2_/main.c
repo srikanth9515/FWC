8739abe5b8dc2b299aaf48ea7a5e1dc8ca763747
“srikanth9515” <ssrikanthreddy03@gmail.com>
2023-02-13 14:00:10 +0530

    hello

---
+++ vectors/10/7.2/6/c codes_2_/main.c
@@ -0,0 +1,57 @@
+
+/* Code by : S SRIKANTH REDDY FWC220107
+  Date : Feb 12 2023
+Released under : Dr. G. V. V. Sharma sir
+This is code for Prooving the properties of parallelogram
+Code is free any one can use 
+https://github.com/srikanth9515/FWC/blob/main/LICENSE.md
+  */
+#include<stdio.h>  // including stdio.h it is used for standard input and output functions.
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include"sri.h"  // This is the header file which includes all the math functions
+
+
+//main function
+int main()
+{
+char a[10]="1,2";      // creating the points on parallelogram
+char b[10]="4,y";
+char c[10]="3,5";
+char d[10]="x,6";
+printf("a=%s",a);         //printing points
+printf("\n");
+printf("b=%s",b);
+printf("\n");
+printf("c=%s",c);
+printf("\n");
+printf("d=%s",d);
+printf("\n");
+int a1[10]={1,2};
+int b1[10]={4,3};          //initialising an array
+int c1[10]={3,5};
+int d1[10]={6,6};
+pmf ("a.dat",a1,2);//printing the matrix in file
+pmf ("b.dat",b1,2);
+pmf ("c.dat",c1,2);
+pmf ("d.dat",d1,2);
+
+
+printf("Solve for x :");  
+int e,f,g;    //coefcients of algebraic equations for solving x
+e=1;
+f=-3;
+g=3;
+int x=solve(e,f,g); //function for solving x from linear equation
+printf("%d",x);
+printf("\n");
+printf("solve for y : ");
+int l=1,m=-2,n=1;        // //coefcients of algebraic equations for solving y
+int y=solve(l,m,n); //function for solving y from linear equation
+printf("%d",y);
+
+
+return 0;
+}
+
---
+++ vectors/10/7.2/6/c codes_2_/sri.h
@@ -0,0 +1,47 @@
+double **createMat1(int m,int n);
+double *createMat(int m); // function for creating an array
+void pmf(char *str, int *a,int len); //function for printing matrix in file
+int solve(int a,int b,int c); //Function for solving algebraic expression of having one variqble.
+
+
+int solve(int a,int b,int c)  //Function for solving algebraic expression of having one variqble.
+{
+
+	return (c-b)/a;
+}
+
+//Defining the function for matrix creation
+double **createMat1(int m,int n)
+{
+ int i;
+ double **a;
+ 
+ //Allocate memory to the pointer
+a = (double **)malloc(m * sizeof( *a));
+    for (i=0; i<m; i++)
+         a[i] = (double *)malloc(n * sizeof( *a[i]));
+
+ return a;
+}
+//End function for matrix creation
+
+double *createMat(int m) // function for creating an array
+{
+ int i;
+ double *a;
+ 
+ //Allocate memory to the pointer
+a = (double *)malloc(m * sizeof(double));
+ return a;
+}
+void pmf(char *str, int *a,int len)  //function for printing matrix in file
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%d\n",a[i]);
+}
+}
