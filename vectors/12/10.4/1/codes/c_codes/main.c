5f7c288c0dfa55628e08d97798b4de286416a90f
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-02-16 14:32:15 +0530

    Update main.c

--- vectors/12/10.4/1/codes/c_codes/main.c
+++ vectors/12/10.4/1/codes/c_codes/main.c
@@ -1,13 +1,11 @@
 
 /* Code by : S SRIKANTH REDDY FWC220107
-  Date : Feb 12 2023
+  Date : Feb 16 2023
 Released under : Dr. G. V. V. Sharma sir
 This is code for doing doing the cross product and doing the norm for the samee
 Code is free any one can use 
 https://github.com/srikanth9515/FWC/blob/main/LICENSE.md
   */
-
-
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,37 +15,33 @@ https://github.com/srikanth9515/FWC/blob/main/LICENSE.md
 //main function
 int main()
 {
-	double *a,*b,*c,*d;    //Declaring an arrary by using malloc
+	double *a,*b,*c,*d;        //Declaring an arrary by using malloc
 	int n=3;
 	int i;
-	a=createMat(n);
+	a=createMat(n);      	    //creating vectors a,b,c
 	b=createMat(n);
 	c=createMat(n);
-
+	
         printf("Enter the 1 st vector : ");
-        readMat(a,3);
+        readMat(a,3);		   //Reading the vector a
         printf("Enter the 2nd vector : ");
-        readMat(b,3);
+        readMat(b,3);         	   //Reading the vector a
 
-
-	d=cross(a,b,c);             //Function for doing cross product
+	d=cross(a,b,c);           //Function for finding cross product
 	printf("Cross product is : \n");
 	print(d,3);
 
 	double norm;
-	norm=linalg_norm(d,n);  //Function for doing norm
+	norm=linalg_norm(d,n);     //Function for doing norm
 
-	printf("The norm is : ");
+	printf("The norm is : ");  //printing the norm value
 	printf("%lf",norm);
 
-	pmf("a.dat",a,2);
+	pmf("a.dat",a,2);         //creating the files for a, b,c
 	pmf("b.dat",b,2);
 	pmf("c.dat",d,2);
 
 	
-
-
-
 }
 
 
