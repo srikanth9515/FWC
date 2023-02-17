c015d4046bd26c91e478080f032a547cd152065f
“srikanth9515” <ssrikanthreddy03@gmail.com>
2023-02-17 07:23:53 +0530

    hello

--- matrices/lines/11.10.2.4/c_codes/main.c
+++ matrices/lines/11.10.2.4/c_codes/main.c
@@ -2,16 +2,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include"gvv.h"
+#include"sr.h"
 int main()
 {
-	double **m; //double pointer for 2d matrix
-	int s=2; //size of array
+	double **m;          //double pointer for 2d matrix
+	int s=2;             //size of array
 	int i,j,n1=1;
 	m=createMat1(2,1);  // created a 2*1 array
 	m[0][0]=1;
 	m[1][0]=-2;
-	printf("The vector m is :\n");
+	printf("The vector m is :\n");//printing m value
 	for(i=0;i<s;i++)
 	{
 		for(j=0;j<n1;j++)
@@ -19,19 +19,19 @@ int main()
 		printf("%lf\n",m[i][j]);
 		}
 	}
-	double **omat;
+	double **omat;         //finding omat 
 	omat=createMat1(2,2);  //creating 2*2 matrix
-	omat[0][0]=0;
+	omat[0][0]=0;          //omat values
 	omat[0][1]=1;
 	omat[1][0]=-1;
 	omat[1][1]=0;
-	double **A;
-	A=createMat1(2,1);
-	A[0][0]=-3;
+	double **A;           //finding  vector point  A 
+	A=createMat1(2,1);    // creating a 2*1 array
+	A[0][0]=-3;         
 	A[1][0]=0;
 
 
-printf("Omat is :\n");
+printf("Omat is :\n");        //printing omat values
 for(i=0;i<s;i++)
 {
 	for(j=0;j<s;j++)
@@ -41,9 +41,9 @@ for(i=0;i<s;i++)
 	printf("\n");
 	
 }
-double **n;
+double **n;                     //creating vector n
 n=createMat1(2,1);
-n=matmul(omat,m,2,2,1);   //finding normal vector
+n=matmul(omat,m,2,2,1);         //finding normal vector
 printf("The vector n is :\n");
 for(i=0;i<s;i++)
 {
@@ -54,17 +54,10 @@ for(i=0;i<s;i++)
 	printf("\n");
 }
 
-	pmf("m.dat",m,2,1);  // printing the matrix in file.
+	pmf("m.dat",m,2,1);     // printing the matrix in file.
 	pmf("n.dat",n,2,1);
 	pmf("omat.dat",omat,2,2);
 	pmf("A.dat",A,2,1);
-
-
-
-
-
-
-
 
 
 
