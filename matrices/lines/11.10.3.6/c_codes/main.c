f42987500543fd0e8000f1114f3bc6507217aeb3
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-02-17 07:53:54 +0530

    Update main.c

--- matrices/lines/11.10.3.6/c_codes/main.c
+++ matrices/lines/11.10.3.6/c_codes/main.c
@@ -1,3 +1,12 @@
+
+/* Code by : S SRIKANTH REDDY FWC220107
+  Date : Feb 17 2023
+Released under : Dr. G. V. V. Sharma sir
+This is code for finding the third coordinte in a straight line
+Code is free any one can use 
+https://github.com/srikanth9515/FWC/blob/main/LICENSE.md
+  */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
