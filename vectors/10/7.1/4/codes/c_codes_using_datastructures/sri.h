0b22df0f67a4ea9176c11e2908545fb6765c25c1
srikanth9515 <119584838+srikanth9515@users.noreply.github.com>
2023-03-02 17:05:05 +0530

    Add files via upload

--- vectors/10/7.1/4/codes/c_codes_using_datastructures/sri.h
+++ vectors/10/7.1/4/codes/c_codes_using_datastructures/sri.h
@@ -1,111 +1,244 @@
 
-struct Node
-{
-    int data;
+typedef struct Node {
+    int value;
+    int row;
+    int col;
     struct Node* next;
-};
+} Node;
 
+Node* createNode(int value, int row, int col); 
+void insert_node(Node **head, int row, int col, int val);
+Node* createMatrix(int numRows, int numCols);
+void print(Node *head);
+Node *sub(Node *a, Node *b);
+Node *transpose_matrix(Node *head);
+double Norm(Node* head, int numRows, int numCols);
+void check(double d1,double d2,double d3);
+void save(Node *head,char *str,int r,int c);
 
-
-void push(struct Node** head_ref, int new_data);
-int *subtractLists(struct Node* l1, struct Node* l2);
-int norm(struct Node* l1);
-void check(double n1,double n2,double n3);
-void print(int *A,int n);
-int *createMat(int m);
-void readMat(int *p, int m);
-void pmf(char *str, int *a,int len); //function for printing matrix in file
-
-
-void print(int *A,int n)
+Node* createNode(int value, int row, int col) 
 {
-	int i;
-	for(i=0;i<n;i++)
-	{
-		printf("%d",A[i]);
-	}
-}
-void pmf(char *str, int *a,int len)  //function for printing matrix in file
-{
-int i;
-FILE *fp;
-
-fp = fopen(str,"w");
-for (i = 0; i < len; i++)
-{
-fprintf(fp,"%d\n",a[i]);
-}
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->value = value;
+    newNode->row = row;
+    newNode->col = col;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void check(double n1,double n2,double n3)
-{
-    if(n1==n2 || n1==n3)
-    {
-	    printf("The given coordinates are isoceles triangle");
-    }
-    else
-    {
-	    printf("The given coordinates are not belongs to isoceles triangle");
+void insert_node(Node **head, int row, int col, int val) {
+    Node *new_node = (Node *) malloc(sizeof(Node));
+    new_node->row = row;
+    new_node->col = col;
+    new_node->value = val;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+        return;
     }
 
-
-}
-int *createMat(int m)
-{
-	int *A;
-	A=(int *)malloc(m*sizeof(int));
-
-	return A;
-}
-
-void readMat(int *A,int n)
-{
-	int i;
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&A[i]);
-	}
-}
-// function for pusing the values in stacks using linked lists
-void push(struct Node** head_ref, int new_data)
-{
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-    new_node->data = new_data;
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-// function for subtraction of two linked lists
-int *subtractLists(struct Node* l1, struct Node* l2) 
-
-{
-	int *A;
-	A=(int *)malloc(2*sizeof(int));
-    struct Node *temp1 = l1, *temp2 = l2;
-    int diff,i=0;
-    while (temp1 != NULL && temp2 != NULL)
-    {
-        diff = temp1->data - temp2->data;
-	A[i]=diff;
-        printf("%d ", diff);
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-	i++;
+    Node *current = *head;
+    while (current->next != NULL) {
+        current = current->next;
     }
 
-    return A;
+    current->next = new_node;
+}
+// Function to create a matrix using linked lists
+Node* createMatrix(int numRows, int numCols) {
+    Node* head = NULL;
+    Node* currentRow = NULL;
+
+    // Loop through each row
+    for (int i = 0; i < numRows; i++) {
+        Node* newRow = NULL;
+        Node* currentCol = NULL;
+
+        // Loop through each column in the current row
+        for (int j = 0; j < numCols; j++) {
+            int value;
+            printf("Enter the value at position (%d,%d): ", i, j);
+            scanf("%d", &value);
+
+            // If the value is not 0, create a new node and add it to the matrix
+            if (value != 0) {
+                Node* newNode = createNode(value, i, j);
+                if (newRow == NULL) {
+                    newRow = newNode;
+                    currentCol = newNode;
+                } else {
+                    currentCol->next = newNode;
+                    currentCol = newNode;
+                }
+            }
+        }
+
+        // If the current row is not empty, add it to the matrix
+        if (newRow != NULL) {
+            if (head == NULL) {
+                head = newRow;
+                currentRow = newRow;
+            } else {
+                currentRow->next = newRow;
+                currentRow = newRow;
+            }
+        }
+    }
+
+    return head;
 }
 
-// function for finding norm using linked lists
-int norm(struct Node* l1)
-{
-	struct Node *temp1 = l1;
-	double norm=0.0;
-	double result;
-	while(temp1!=NULL)
-	{
-		norm=norm+(temp1->data)*(temp1->data);
-		temp1 = temp1 -> next;
-	}
-	result=sqrt(norm);
-	return result;
+
+
+
+void print(Node *head) {
+    if (head == NULL) {
+        printf("Empty matrix\n");
+        return;
+    }
+
+    int max_row = head->row;
+    int max_col = head->col;
+
+    Node *current = head;
+    while (current != NULL) {
+        if (current->row > max_row) {
+            max_row = current->row;
+        }
+        if (current->col > max_col) {
+            max_col = current->col;
+        }
+        current = current->next;
+    }
+
+    for (int i = 0; i <= max_row; i++) {
+        for (int j = 0; j <= max_col; j++) {
+            current = head;
+            int val;
+            while (current != NULL) {
+                if (current->row == i && current->col == j) {
+                    val = current->value;
+                    break;
+                }
+                current = current->next;
+            }
+            printf("%d ", val);
+        }
+        printf("\n");
+    }
 }
+
+
+
+Node *sub(Node *a, Node *b) {
+    Node *result = NULL;
+
+    while (a != NULL && b != NULL) {
+        if (a->row < b->row || (a->row == b->row && a->col < b->col)) {
+            insert_node(&result, a->row, a->col, a->value);
+            a = a->next;
+        } else if (a->row == b->row && a->col == b->col) {
+            int val = a->value - b->value;
+            if (val != 0) {
+                insert_node(&result, a->row, a->col, val);
+            }
+            a = a->next;
+            b = b->next;
+        } else {
+            insert_node(&result, b->row, b->col, -b->value);
+            b = b->next;
+        }
+    }
+
+    while (a != NULL) {
+        insert_node(&result, a->row, a->col, a->value);
+        a = a->next;
+    }
+
+    while (b != NULL) {
+        insert_node(&result, b->row, b->col, -b->value);
+        b = b->next;
+    }
+
+    return result;
+}
+
+Node *transpose_matrix(Node *head) {
+    Node *result = NULL;
+    Node *current = head;
+
+    while (current != NULL) {
+        insert_node(&result, current->col, current->row, current->value);
+        current = current->next;
+    }
+
+    return result;
+}
+
+
+            // If the dot product is not 0, create a new node and add it to the matrix
+
+
+double Norm(Node* head, int numRows, int numCols)
+{
+    double sumOfSquares = 0;
+
+    // Loop through each column
+    for (int j = 0; j < numCols; j++) 
+    {
+        double colSum = 0;
+
+        // Loop through each element in the column
+        Node* current = head;
+        while (current != NULL) 
+	{
+            if (current->col == j) 
+	    {
+                colSum += pow(current->value, 2);
+            }
+            current = current->next;
+        }
+
+        sumOfSquares += colSum;
+    }
+    return sqrt(sumOfSquares);
+}
+
+
+void check(double d1,double d2,double d3)
+{
+	if(d1==d2 || d1 == d3)
+	{
+		printf("The triangle is Isoceles triangle ");
+	}
+	else
+	{
+		printf("The triangle is not Isoceles triangle ");
+	}
+}
+
+void save(Node *head,char *str,int r,int c)
+{
+	FILE *fp;
+	fp = fopen(str,"w");
+        Node* current = head;
+	int i,j;
+	while(current!=NULL)
+	{
+	//for(i=0;i<r;i++)
+	//{
+		for(j=0;j<c;j++)
+		{
+			fprintf(fp,"%d\n",current->value);
+		}
+	//}
+            current = current->next;
+	}
+}
+
+
+
+
+
+
