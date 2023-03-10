d6ca3052e85ac6a7c29f6d90f44dafaa2b4d7680
srikanth9515 <ssrikanthreddy03@gmail.com>
2023-03-10 14:10:16 +0000

    hello

---
+++ iot/codes/flash.sh
@@ -0,0 +1,1 @@
+pio run -t nobuild -t upload --upload-port 192.168.100.47
---
+++ iot/codes/src/main.cpp
@@ -0,0 +1,58 @@
+//----------------------Skeleton Code--------------------//
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+//    Can be client or even host   //
+#ifndef STASSID
+#define STASSID "srikanth"  // Replace with your network credentials
+#define STAPSK  "srikanth123"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+int X,Y,Z,D;
+
+void OTAsetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+}
+
+void OTAloop() {
+  ArduinoOTA.handle();
+}
+
+//-------------------------------------------------------//
+
+void setup(){
+  OTAsetup();
+
+  //-------------------//
+  // Custom setup code //
+  //-------------------//
+  pinMode(2, INPUT);
+  pinMode(4, INPUT);  
+  pinMode(6, INPUT);  
+  pinMode(8, OUTPUT);  
+}
+
+void loop() {
+  OTAloop();
+  delay(10);  // If no custom loop code ensure to have a delay in loop
+  //-------------------//
+  // Custom loop code  //
+  //-------------------//
+  X=digitalRead(2);
+  Y=digitalRead(4);
+  Z=digitalRead(6);
+  D=((X&&Y)||(Y&&Z));
+  digitalWrite(8, D);
+}
+
+
+
